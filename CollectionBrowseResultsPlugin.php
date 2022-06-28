f445ada0eff07bd18798f0e3a551db41c9f5b772
Austin Mason <austin.pj.mason@gmail.com>
2022-06-28 10:49:42 -0500

    Added ItemsBrowsePerPage filter

--- CollectionBrowseResultsPlugin.php
+++ CollectionBrowseResultsPlugin.php
@@ -19,6 +19,7 @@ class BrowseResultsPaginationPlugin extends Omeka_Plugin_AbstractPlugin
 
 protected $_filters = array(
     'collections_browse_per_page',
+    'items_browse_per_page',
 );
 
 public function filterCollectionsBrowsePerPage($perPage, $args)
@@ -27,4 +28,10 @@ public function filterCollectionsBrowsePerPage($perPage, $args)
     return $totalrecs;
 }
 
+public function filterItemsBrowsePerPage($perPage, $args) 
+{
+    $totalrecs = total_records('Items');
+    return $totalrecs;
+}    
+    
 }
